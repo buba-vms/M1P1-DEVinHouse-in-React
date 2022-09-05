e6d0d2a5f260d68f16af3cb75a279aa0e2caa193
buba-vms <victor.buba@live.com>
2022-09-05 15:49:41 -0300

    teste deploy

--- src/components/routes/RotaPrincipal.js
+++ src/components/routes/RotaPrincipal.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../../pages/Home';
-import PageNotFound from '../../pages/PageNotFound';
+// import PageNotFound from '../../pages/PageNotFound';
 const RotaPrincipal = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="*" element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="/" element={<Navigate replace to="home" />} />
       </Routes>
